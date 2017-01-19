4a17472e53ec6e34f42a313ee1413a66032c86fd
disne_y <bold.disney@gmail.com>
2017-01-19 14:07:02 +0300

    Fix bugs, update test case

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,12 +1,12 @@
 class OrderNotifier < ApplicationMailer
-  dafault from: 'dave <eShop@example.com>'
+  default from: 'dave <eshop@example.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_notifier.received.subject
   #
-  def received
+  def received(order)
     @order = order
 
     mail to: order.email, subject: 'eShop Store Order Confirmation'
