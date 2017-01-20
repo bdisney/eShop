5bca0eff24d4aae2dc3a6a971550855fbb4392c3
disne_y <bold.disney@gmail.com>
2017-01-20 19:14:00 +0300

    Add Contact_email and message validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authorize, only: [:new, :create]
 
   # GET /contacts
   # GET /contacts.json
@@ -28,24 +29,10 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.html { redirect_to store_url, notice: 'Thank you for your message!' }
         format.json { render :show, status: :created, location: @contact }
       else
         format.html { render :new }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /contacts/1
-  # PATCH/PUT /contacts/1.json
-  def update
-    respond_to do |format|
-      if @contact.update(contact_params)
-        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
-        format.json { render :show, status: :ok, location: @contact }
-      else
-        format.html { render :edit }
         format.json { render json: @contact.errors, status: :unprocessable_entity }
       end
     end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ActiveRecord::Base
+  validates :email, presence: true
+  validates :message, presence: true
+
+  validates_length_of :message, :minimum => 3
+  validates_length_of :message, :maximum => 140
 end
