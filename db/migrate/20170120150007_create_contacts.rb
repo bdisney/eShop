3241576cc14212b7fdba456b7c62e5456b1247f3
disne_y <bold.disney@gmail.com>
2017-01-20 18:01:24 +0300

    Scaffold Contact

---
+++ db/migrate/20170120150007_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.email :email
+      t.text :message
+
+      t.timestamps null: false
+    end
+  end
+end
