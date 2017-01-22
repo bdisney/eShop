62e6a51c091bbc644c26028de9ff95a80163c8bc
disne_y <bold.disney@gmail.com>
2017-01-22 15:56:42 +0300

    Add product_title_length validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ActiveRecord::Base
   before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, presence: true
+  validates_length_of :title, :minimum => 5
+  validates_length_of :title, :maximum => 60
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
