2f6d88bc1a46af50ae2c68f732f31747a70df19c
disne_y <bold.disney@gmail.com>
2017-01-24 12:08:01 +0300

    Add title, subtitle, body, image_url validation for News

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,13 @@
 class News < ActiveRecord::Base
+  validates :title, :sub_title, :body, :image_url, presence: true
+  validates_length_of :title, :minimum => 5
+  validates_length_of :title, :maximum => 60
+
+  validates_length_of :sub_title, :minimum => 5
+  validates_length_of :sub_title, :maximum => 60
+
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
