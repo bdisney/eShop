2148d0184d18c1d83849851f223b42fa2a57eb96
disne_y <bold.disney@gmail.com>
2017-01-25 14:21:17 +0300

    Add user_registration details validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :second_name, :email, presence: true
+  validates_length_of :first_name, :minimum => 3
+  validates_length_of :second_name, :minimum => 3
+
+  validates_length_of :password, :minimum => 6
+  
 end
