d91662a8b8df15d54a8fe550d8071582a2215f85
disne_y <bold.disney@gmail.com>
2017-02-04 22:06:45 +0300

    Add current_cart method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,16 @@ class ApplicationController < ActionController::Base
     redirect_to store_url, :alert => exception.message
   end
 
+  private
+
+  def current_cart
+      Cart.find(session[:cart_id])
+      rescue ActiveRecord::RecordNotFound
+        
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 
   #protected
     def authorize
