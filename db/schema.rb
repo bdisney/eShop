cbbff0fb5e805b95c1bef4077ecbcbbbb76864f0
disne_y <bold.disney@gmail.com>
2017-02-10 13:58:25 +0300

    Add user_id column to Review table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209165600) do
+ActiveRecord::Schema.define(version: 20170210094109) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20170209165600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "product_id"
+    t.integer  "user_id"
   end
 
   create_table "sub_categories", force: :cascade do |t|
