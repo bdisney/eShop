8590dcdf407d034b2f2abfff1201ddb7dd250eac
disne_y <bold.disney@gmail.com>
2017-02-13 14:15:14 +0300

    Add about method to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,23 @@
 class PagesController < ApplicationController
-  def show
-    render template: "pages/#{params[:page]}"
+  include CurrentCart
+  skip_before_action :authenticate_user!
+  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  skip_authorization_check
+  
+  def about
+    @cart = current_cart
   end
+
+  def show
+    if valid_page?
+      render template: "pages/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
+  private
+    def valid_page?
+      File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
+    end
 end
