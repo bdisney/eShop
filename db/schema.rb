cc33ff843d9267f4cad73f9806979171ec210970
disne_y <bold.disney@gmail.com>
2017-02-14 13:33:36 +0300

    Add category_id to Products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212152440) do
+ActiveRecord::Schema.define(version: 20170214103213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20170212152440) do
     t.datetime "updated_at",                                               null: false
     t.integer  "main_category_id"
     t.boolean  "bestseller",                               default: false
+    t.integer  "category_id"
   end
 
   create_table "reviews", force: :cascade do |t|
