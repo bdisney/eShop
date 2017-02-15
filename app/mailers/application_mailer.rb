e4aed94b80ff90124b867d91057ab82f3180f412
disne_y <bold.disney@gmail.com>
2017-02-15 17:22:39 +0300

    Turn off order notifier

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "bold.disney@gmail.com"
+  default from: "example@example.com"
   layout 'mailer'
 end
--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ApplicationMailer
-  default from: 'Denis <bold.disney@gmail.com>'
+  default from: 'Denis <example@example.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
