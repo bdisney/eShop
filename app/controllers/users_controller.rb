e0948e821a38131dc60c9b1cc263795ef9f8b750
disne_y <bold.disney@gmail.com>
2017-02-16 21:47:26 +0300

    Fix bug in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -86,6 +86,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :second_name, :username, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
     end
 end
