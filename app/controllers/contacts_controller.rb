6dd5de7b6c2633f54d3aaf35226783fa536fab6c
disne_y <bold.disney@gmail.com>
2017-02-18 16:51:09 +0300

    Minor fixes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -63,6 +63,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:email, :message)
+      params.require(:contact).permit(:email, :message, :name, :subject)
     end
 end
