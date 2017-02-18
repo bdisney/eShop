fbcc3c86bd0ab78bbee93f96b309bcff9c48a2c9
disne_y <bold.disney@gmail.com>
2017-02-18 15:28:53 +0300

    Add contact name and subject validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 class Contact < ActiveRecord::Base
   validates :email, presence: true
   validates :message, presence: true
+  validates :name, presence: true
+  validates :subject, presence: true
 
   validates_length_of :message, :minimum => 3
   validates_length_of :message, :maximum => 140
