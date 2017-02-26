35dae8141fbda910ed080a25611e812907fea05c
disne_y <bold.disney@gmail.com>
2017-02-26 17:19:13 +0300

    Implement devise_helper for devise_error_messages

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,25 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" unless devise_error_messages?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:div, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="error_explanation">
+      <script type="text/javascript">
+        toastr.error('#{messages}') 
+      </script>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+
+end
