f01fb89f4f5815dace268dfab2bc6d554067141e
disne_y <bold.disney@gmail.com>
2017-02-26 17:41:07 +0300

    Fix incorrect 'from' address in devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'just-watches-ror.tk'
+  config.mailer_sender = '"JW team. Reset Password Instructions" <no-reply@just-watches-ror.tk>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
